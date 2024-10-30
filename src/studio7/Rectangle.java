21945c94831ca2b9db50d84b501970a50379933d
huyly2028 <huyly2006@gmail.com>
2024-10-30 15:20:19 -0500

    Done with Rectangle

---
+++ src/studio7/Rectangle.java
@@ -0,0 +1,48 @@
+package studio7;
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Rectangle {
+	private double length;
+	private double width;
+	
+	public Rectangle(double length, double width) {
+		this.length=length;
+		this.width=width;
+	}
+	
+	public double calculateArea() {
+		double area = length*width;
+		return area;
+	}
+
+	public boolean isSquare() {
+		if (length==width)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isBiggerThan(double otherRectangleArea) {
+		if(calculateArea()>otherRectangleArea) 
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public void drawRectangle() {
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.rectangle(.5,.5,length,width);
+
+	}
+	public static void main(String[]args) {
+		Rectangle rectangle1 = new Rectangle(0.25,0.25);
+		Rectangle rectangle2 = new Rectangle(0.7,0.9);
+
+		rectangle1.calculateArea();
+		rectangle2.isBiggerThan(rectangle1.calculateArea());
+		rectangle1.drawRectangle();
+	}
+		
+}
---
+++ src/studio7/WorkBench.java
@@ -0,0 +1,11 @@
+package studio7;
+import 
+public class WorkBench {
+	public static void main(String[]args) {
+		
+		
+		
+		
+	}
+
+}
